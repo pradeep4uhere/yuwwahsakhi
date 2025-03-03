8165b5ce1b9eadea4c50ea290845e0fbb1c64a3d
pradeep4uhere <kumar031984@gmail.com>
2025-03-03 17:30:09 +0530

    Update partner

--- database/migrations/2025_02_05_062048_add_password_to_partners_table.php
+++ database/migrations/2025_02_05_062048_add_password_to_partners_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('partners', function (Blueprint $table) {
+        Schema::table('partner_centers', function (Blueprint $table) {
             $table->string('password')->after('email'); 
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('partners', function (Blueprint $table) {
+        Schema::table('partner_centers', function (Blueprint $table) {
             $table->dropColumn('password');
         });
     }
