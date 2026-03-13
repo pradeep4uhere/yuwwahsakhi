4cd61cad325775650bed543fe26e7cc8a1c2ac3c
Siya <siya@example.com>
2026-03-13 12:05:32 +0530

    update

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -453,13 +453,24 @@ class PartnerController extends Controller
 
         $eventList = DB::table('event_transactions as et')
         ->leftJoin('yuwaah_sakhi as ys', 'et.ys_id', '=', 'ys.id')
+        ->leftJoin('learners as l', 'et.learner_id', '=', 'l.id')
+        ->leftJoin('learner_courses as lc', 'lc.phone_number', '=', 'et.beneficiary_phone_number')
         ->leftJoin('yuwaah_event_masters as em', 'em.id', '=', 'et.event_category')
         ->where('ys.partner_id', $partnerId)
         ->where('ys.csc_id','!=','Sandbox_Testing')
         ->whereNotNull('et.review_status')
         ->whereNotNull('et.learner_id')
         ->whereNotNull('et.event_date_submitted')
-        ->select('et.*', 'em.event_category','ys.csc_id','ys.sakhi_id')
+        ->select('et.*',
+        DB::raw("CASE WHEN l.DIFFRENTLY_ABLED = 1 THEN 'Yes' ELSE 'No' END as DIFFRENTLY_ABLED"),
+        'l.USER_MARIAL_STATUS',
+        'l.RELIGION',
+        'l.education_level',
+        'l.digital_proficiency',
+        'l.english_knowledge',
+        DB::raw("CASE WHEN l.interested_in_opportunities = 1 THEN 'Yes' ELSE 'No' END as interested_in_opportunities"),
+        'lc.course_name',
+        'em.event_category','ys.csc_id','ys.sakhi_id')
         ->paginate(50);
 
             
--- resources/views/partner/event/list.blade.php
+++ resources/views/partner/event/list.blade.php
@@ -9,7 +9,7 @@
         <td style="text-align: right; font-size:14px;font-weight:bold"><a href="{{ route('partner.event.export') }}"><img src="{{ asset('asset/images/download.png') }}" alt="Download" style="height:16px; vertical-align:middle;"> Export Event Transactions</a></td>
     </tr>
 </table>
-        <div class="table-container">
+    <div class="table-responsive">
         <table class="table table-striped table-bordered">
         <thead class="table-dark">
             <tr>
@@ -17,15 +17,25 @@
                 <th>Agent ID</th>
                 <th>Unit Institute</th>
                 <th>Event Name</th>
-                <th>Event Category</th>
-                <th>Beneficiary Name</th>
-                <th>Beneficiary Number</th>
-                <th>Event Status</th>
-                <th>Event Submitted</th>
-                <th>Documents</th>
+                <th nowrap="nowrap">Event Category</th>
+                <th nowrap="nowrap">Beneficiary Name</th>
+                <th nowrap="nowrap">Beneficiary Number</th>
+                <th nowrap="nowrap">Event Status</th>
+                <th nowrap="nowrap">Event Submitted</th>
+                <th nowrap="nowrap">Documents</th>
+                <th nowrap="nowrap">Differently Abled</th>
+                <th nowrap="nowrap">Education Level</th>
+                <th nowrap="nowrap">Course Name</th>
+                <th nowrap="nowrap">Marital Status</th>
+                <th nowrap="nowrap">Religion</th>
+                <th nowrap="nowrap">Digital Proficiency Level</th>
+                <th nowrap="nowrap">English Proficiency Level</th>
+                <th nowrap="nowrap">Interested Opportunities</th>
+                <th nowrap="nowrap">Function Type</th>
+                <th nowrap="nowrap">Industry Type</th>
             
-                <th>Comment</th>
-                <th>Updated</th>
+                <th nowrap="nowrap">Comment</th>
+                <th nowrap="nowrap">Updated</th>
             </tr>
         </thead>
         <tbody>
@@ -41,13 +51,13 @@
                 <td nowrap="nowrap">{{ $item->sakhi_id }}</td>
                 <td nowrap="nowrap">{{ $item->csc_id }}</td>
                 <td nowrap="nowrap">{{ $item->event_name }}</td>
-                <td>{{ $item->event_category }}</td>
-                <td>{{ $item->beneficiary_name }}</td>
-                <td>{{ $item->beneficiary_phone_number }}</td>
-                <td>{{ $item->review_status }}</td>
+                <td nowrap="nowrap">{{ $item->event_category }}</td>
+                <td nowrap="nowrap">{{ $item->beneficiary_name }}</td>
+                <td nowrap="nowrap">{{ $item->beneficiary_phone_number }}</td>
+                <td nowrap="nowrap">{{ $item->review_status }}</td>
                
-                <td>{{ $item->event_date_submitted }}</td>
-                <td>
+                <td nowrap="nowrap">{{ $item->event_date_submitted }}</td>
+                <td nowrap="nowrap">
                 @if(!empty($document))
                         @foreach ($document as $doc)
                             <a href="{{ asset('storage/'.$doc) }}" target="_blank">View</a><br>
@@ -57,6 +67,16 @@
                         <span>No Document</span>
                     @endif
                 </td>
+                <th nowrap="nowrap">{{$item->DIFFRENTLY_ABLED}}</th>
+                <th nowrap="nowrap">{{$item->education_level}}</th>
+                <th nowrap="nowrap">{{$item->course_name}}</th>
+                <th nowrap="nowrap">{{$item->USER_MARIAL_STATUS}}</th>
+                <th nowrap="nowrap">{{$item->RELIGION}}</th>
+                <th nowrap="nowrap">{{$item->digital_proficiency}}</th>
+                <th nowrap="nowrap">{{$item->english_knowledge}}</th>
+                <th nowrap="nowrap">{{$item->interested_in_opportunities}}</th>
+                <th nowrap="nowrap">{{$item->field_type}}</th>
+                <th nowrap="nowrap">{{$item->industry_type}}</th>
                 <!-- <td nowrap="nowrap">
                 @if(!empty($document))
                      @foreach ($document as $doc)
@@ -78,7 +98,7 @@
                         Comment
                     </a>
                 </td>
-                <td>{{ $item->updated_at }}</td>
+                <td nowrap="nowrap">{{ $item->updated_at }}</td>
             </tr>
             <?php $count++; ?>
             @endforeach
