30419433fe5cf58ad9534a2bedfc41f00c4e8cab
Siya <siya@example.com>
2026-04-17 10:42:59 +0530

    update

--- app/Exports/PlacementPartnerLearnersEventExport.php
+++ app/Exports/PlacementPartnerLearnersEventExport.php
@@ -34,7 +34,7 @@ class PlacementPartnerLearnersEventExport implements FromCollection, WithHeading
                 'ys.csc_id',
                 'ys.sakhi_id',
                 'et.event_name',
-                'em.event_category',
+                'et.event_category',
                 'et.beneficiary_name',
                 'et.beneficiary_phone_number',
                 'et.review_status',
